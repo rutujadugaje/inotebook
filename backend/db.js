9672e17c3a892db236efdb1ae704ac2d53d4b695
rutujadugaje <dugajerutuja@gmail.com>
2025-04-28 23:18:27 -0700

    MongoDB setup complited

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/inotebook";
+
+const connectToMongo = async () => {
+    try{
+        await mongoose.connect(mongoURI);
+        console.log("Connected to MongoDB Succcessfully");
+    }   catch(error){
+        console.error("MongoDB Connection Error:", error)
+    } 
+};
+
+module.exports = connectToMongo;
