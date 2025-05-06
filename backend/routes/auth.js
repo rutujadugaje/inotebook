92af4eb25f859760a22a47ac15f63726f5914ecc
rutujadugaje <dugajerutuja@gmail.com>
2025-05-06 02:36:07 -0700

    install bcryptjs package for high security

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const auth = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'Harryisagoodboy';
 
 //Create a user using POST "/api/auth/createuser". Doesn't require Auth
 auth.post('/createuser', [
@@ -23,14 +26,24 @@ auth.post('/createuser', [
     return res.status(400).json({ error: "Sorry a user with this email already exists"})
 
   }
+  //Hash the password using bcrypt
+  const salt = await bcrypt.genSalt(10);
+  const securePassword = await bcrypt.hash(req.body.password, salt);
+
   //Create a new user
   user = await User.create({
     name : req.body.name,
-    password : req.body.password,
+    password : securePassword,
     email : req.body.email,
-  })
-  res.json({ user })
-  // res.json({ Success: "User created successfully", user: user })
+  });
+  const data = {
+    user: {
+      id: user.id
+    }
+  }
+  const authtoken = jwt.sign(data , JWT_SECRET);
+  
+  res.json({ authtoken })
 
 } catch (error) {
   console.error(error.message);
---
+++ backend/text.md
@@ -0,0 +1,11 @@
+***install bcryptjs for hashing password***
+```
+npm i bcryptjs
+```
+
+***Json web token***
+```
+ npm install jsonwebtoken
+ ```
+
+ 
